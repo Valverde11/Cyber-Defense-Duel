b3bd66cd0ad934fb7b72594ac4d595fb643a32e3
Andrés Emiliano Aguilar Méndez <a.aguilar.6@estudiantec.cr>
2026-03-19 20:15:09 -0600

    Update GameServer.java
    se añaden imports necesarios para esta clase

--- GameScreen/src/server/GameServer.java
+++ GameScreen/src/server/GameServer.java
@@ -1,5 +1,9 @@
 package server;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class GameServer {
 
     // -------------------------------------------------------------------------
