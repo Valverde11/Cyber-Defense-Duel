c8aaa8d8d180df9ade578e61ba38741a2c8e2c1c
Arky7 <ale23183ale@gmail.com>
2026-03-19 18:32:08 -0600

    avance
    avance por terminar

---
+++ GameScreen/src/main/java/logic/GameConfig.java
@@ -0,0 +1,19 @@
+package logic;
+
+public class GameConfig {
+
+    public int initialHp;
+    public double baseSpawnRate;
+    public double baseAttackSpeed;
+    public int scorePerKill;
+    public int difficultyStepScore;
+    public double spawnMultiplierPerLevel;
+    public double speedAddPerLevel;
+
+    // daño por tipo
+    public int damageYellow;
+    public int damageRed;
+    public int damageBlue;
+}
+
+
